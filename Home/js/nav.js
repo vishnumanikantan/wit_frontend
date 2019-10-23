696266c4a56d42510653f3ab4b2a5ed05a577783
Vishnu M P <mpvishnu12@gmail.com>
2019-10-24 00:53:03 +0530

    Home

---
+++ Home/js/nav.js
@@ -0,0 +1,17 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  $.ajax(
+    {
+      url:'/courses/get-chapters',
+      method:'get',
+
+    }
+  )
+  
